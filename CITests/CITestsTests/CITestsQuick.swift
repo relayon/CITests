eab9e7de30213b6c8095f1d88f7cbb64b259f7e9
alekye <alekye@qq.com>
2017-09-22 15:46:39 +0800

    add pod

---
+++ CITests/CITestsTests/CITestsQuick.swift
@@ -0,0 +1,24 @@
+//
+//  CITestsQuick.swift
+//  CITestsTests
+//
+//  Created by SMC-MAC on 2017/9/22.
+//  Copyright © 2017年 smc. All rights reserved.
+//
+
+//import Quick
+//import Nimble
+//@testable import CITests
+//
+//class CITestsQuick: QuickSpec {
+//    override func spec() {
+//        describe("get 'Utils' level") {
+//            it("has level A") {
+//                let level = Utils.getScoreLevel(score: 90)
+//                expect(level).to(contain("A"))
+////                expect(sections).to(contain("Installing Quick"))
+//            }
+//        }
+//    }
+//}
+
